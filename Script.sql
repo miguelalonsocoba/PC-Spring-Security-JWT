ddfe273da05198e38d7c1032acb77e31215ef925
mac <miguetheking53@gmail.com>
2025-02-15 19:29:52 -0600

    Configurando conexion a DB, y agregando script de la DB

---
+++ Script.sql
@@ -0,0 +1,14 @@
+-- Database: spring_security_jwt
+
+-- DROP DATABASE IF EXISTS spring_security_jwt;
+
+--CREATE DATABASE spring_security_jwt
+  --  WITH
+    --OWNER = postgres
+    --ENCODING = 'UTF8'
+    --LC_COLLATE = 'English_United States.1252'
+    --LC_CTYPE = 'English_United States.1252'
+    --LOCALE_PROVIDER = 'libc'
+    --TABLESPACE = pg_default
+    --CONNECTION LIMIT = -1
+    --IS_TEMPLATE = False;
